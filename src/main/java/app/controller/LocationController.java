7d79410a52641dd47fd421ecd764b2362089a100
vagif4799 <quliyev_vaqif@icloud.com>
2020-01-01 11:15:47 +0400

    -- Create Page is working

--- src/main/java/app/controller/LocationController.java
+++ src/main/java/app/controller/LocationController.java
@@ -1,22 +1,16 @@
 package app.controller;
 
-        import app.model.Message;
-        import org.springframework.beans.factory.annotation.Autowired;
         import org.springframework.stereotype.Controller;
-        import org.springframework.web.bind.annotation.GetMapping;
-        import org.springframework.web.bind.annotation.ResponseBody;
+        import org.springframework.web.bind.annotation.RequestMapping;
+
 
 @Controller
 public class LocationController {
 
-    @Autowired
-    private Message message;
-
-    @GetMapping(path = "/message")
-    @ResponseBody
-    public String message() {
-
-        return message.get();
-
+    @RequestMapping("/showCreate")
+    public String showCreate() {
+        return "createLocation";
     }
+
 }
+
