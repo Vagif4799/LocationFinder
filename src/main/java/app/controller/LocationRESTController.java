ddb696339b241ae3c6dac3a4dc598c96d876f535
vagif4799 <quliyev_vaqif@icloud.com>
2020-01-02 12:12:34 +0400

    -- REST Controller added (Integration Layer)

---
+++ src/main/java/app/controller/LocationRESTController.java
@@ -0,0 +1,44 @@
+package app.controller;
+
+import app.model.Location;
+import app.repository.LocationRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/locations")
+public class LocationRESTController {
+
+    @Autowired
+    LocationRepository locationRepository;
+
+    @GetMapping
+    public List<Location> getLocations() {
+        return locationRepository.findAll();
+    }
+
+    @PostMapping
+    public Location createLocation(@RequestBody Location location) {
+        return locationRepository.save(location);
+    }
+
+    @PutMapping
+    public Location updateLocation(@RequestBody Location location) {
+        return locationRepository.save(location);
+    }
+
+    @DeleteMapping("/{id}")
+    public void deleteLocation(@PathVariable("id") int id) {
+        locationRepository.deleteById(id);
+    }
+
+    @GetMapping("/{id}")
+    public Optional<Location> getLocation(@PathVariable("id") int id) {
+        return locationRepository.findById(id);
+    }
+
+
+}
